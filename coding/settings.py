7460b0182521179282eb4b0bcba3af5559db9a3f
iamakkkhil <iamakhil1408@gmail.com>
2021-08-10 20:24:42 +0530

    static files added

--- coding/settings.py
+++ coding/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fm+fpn=@#)lce(j%%p0onb3cyo&ubgz0n9xop$_re(d&yl+wn'
+SECRET_KEY = os.getenv('djangoKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -146,7 +150,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'noreply.rookeecoders@gmail.com'
-EMAIL_HOST_PASSWORD = 'dpugwruffuknhoxe'
+EMAIL_HOST_PASSWORD = os.getenv('Password')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
